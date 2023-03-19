e30094b9177f7d4ee2f835cfc2c58ba4142c05c0
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-03-19 07:26:57 -0300

    lint changes

--- components/product/BannerWide.tsx
+++ components/product/BannerWide.tsx
@@ -3,13 +3,20 @@ import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Props {
   image: LiveImage;
-  text?: string,
+  text?: string;
   width?: number;
   height?: number;
 }
 
 function BannerWide({ image, text, width = 1100, height = 50 }: Props) {
-  return <div class="relative"><Image src={image} width={width} height={height} class="w-full" /><p class="absolute top-[50%] left-8 text-[32px] text-white uppercase font-bold">{text}</p></div>;
+  return (
+    <div class="relative">
+      <Image src={image} width={width} height={height} class="w-full" />
+      <p class="absolute top-[50%] left-8 text-[32px] text-white uppercase font-bold">
+        {text}
+      </p>
+    </div>
+  );
 }
 
 export default BannerWide;
